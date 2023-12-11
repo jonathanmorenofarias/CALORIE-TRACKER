dadd5d881f9e4c858356f4ea61ca73147d117181
Jonathan Moreno <jonathan.moreno.farias02@gmail.com>
2023-12-10 22:43:14 -0800

    Added a return to the calorie tracker

--- src/pages/MacroForm.jsx
+++ src/pages/MacroForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { FaArrowAltCircleRight } from "react-icons/fa";
 
 const MacroForm = () => {
     const [formData, setFormData] = useState({
@@ -23,35 +24,40 @@ const MacroForm = () => {
     }
 
     return (
-        <form className="macro-data" onSubmit={handleSubmit}>
-            <h1>What is your name?</h1>
-            <input 
-                type="text" 
-                value={formData.name}
-                name="name"
-                onChange={handleChange}
-                placeholder="Enter Name"
-            />
+        <div className="setup-page">
+            <form className="macro-data" onSubmit={handleSubmit}>
+                <h1>What is your name?</h1>
+                <input 
+                    type="text" 
+                    value={formData.name}
+                    name="name"
+                    onChange={handleChange}
+                    placeholder="Enter Name"
+                />
 
-            <h1>How many calories do you want to eat?</h1>
-            <input 
-                type="number"
-                value={formData.calories}
-                name="calories"
-                onChange={handleChange}
-                placeholder="Enter Calories"
-            />
+                <h1>How many calories do you want to eat?</h1>
+                <input 
+                    type="number"
+                    value={formData.calories}
+                    name="calories"
+                    onChange={handleChange}
+                    placeholder="Enter Calories"
+                />
 
-            <h1>What is your protein intake?</h1>
-            <input 
-                type="number"
-                value={formData.protein}
-                name="protein"
-                onChange={handleChange}
-                placeholder="Enter Protein"
-             />
-            <button className="submit-macro">Submit</button>
-        </form>
+                <h1>What is your protein intake?</h1>
+                <input 
+                    type="number"
+                    value={formData.protein}
+                    name="protein"
+                    onChange={handleChange}
+                    placeholder="Enter Protein"
+                />
+                <button className="submit-macro">Submit</button>
+            </form>
+            <h1>Already set this up?</h1>
+            <FaArrowAltCircleRight onClick={() => window.location.href = "/home"} className="already-done" />
+            
+        </div>
     )
 }
 
