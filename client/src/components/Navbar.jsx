2ed41ca4621a856ae43a795f81a79f84dfe63cff
Jonathan Moreno <jonathan.moreno.farias02@gmail.com>
2023-12-15 00:03:28 -0800

    added navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,19 @@
+import MacroLogo from "../images/MacroLogo.png"
+import "../styles/Navbar.css"
+import { Link } from "react-router-dom"
 
-const NavBar = () => {
+const Navbar = () => {
     return (
-        <header>
-
+        <header className="navbar">
+            <img src={MacroLogo} alt="Macro Master Logo" className="macro-logo" onClick={() => window.open("/home", "_self")}/>
+            <ul>
+                <a href="/home"><li>Home</li></a>
+                <a href=""><li>Profile</li></a>
+                <a href=""><li>Meals</li></a>
+                <a href=""><li>Tracker</li></a>
+            </ul>
         </header>
     )
 }
 
-export default NavBar;
+export default Navbar;
